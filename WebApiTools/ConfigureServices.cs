23fc6a954ee94a75b7a9f63e64a63512497f7165
MacroVirus <413859857@qq.com>
2024-05-15 10:28:28 +0800

    新增AgileConfig：配置中心的支持

--- WebApiTools/ConfigureServices.cs
+++ WebApiTools/ConfigureServices.cs
@@ -1,4 +1,9 @@
-﻿using WebApiTools.Infrastructure;
+﻿using AgileConfig.Client;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using WebApiTools.Infrastructure;
 using WebApiTools.TestService;
 using WebApiTools.Tools.Event_Bus;
 
@@ -6,44 +11,13 @@ namespace WebApiTools
 {
     public static class ConfigureServices
     {
-        /// <summary>
-        /// 服务注册
-        /// </summary>
-        /// <param name="services"></param>
-        public static void Configure(IServiceCollection services)
-        {
-            ConfigureEventBus(services);
 
-        }
-
-        #region Event_Bus
-        public static void ConfigureEventBus(IServiceCollection services)
-        {
-            #region 测试EventBus 
-
-            services.AddSingleton<ITestServices, TestServices>();
-            services.AddScoped<Publisher>();
-
-            //注册EventHanders
-            foreach (var subscriberType in EventSubscribers)
-            {
-                //存在一个订阅者订阅多个事件的情况:
-                var baseTypes = subscriberType.GetInterfaces().Where(x => x.IsGenericType && x.GetGenericTypeDefinition() == InterfaceEventSubscriber).ToArray();
-                foreach (var baseType in baseTypes)
-                {
-                    services.AddScoped(baseType, subscriberType);
-
-                }
-            }
-            #endregion
-
-        }
-
-
-        #endregion
         static readonly object _lock = new();//锁
         static readonly Type InterfaceEventSubscriber = typeof(IEventSubscriber<>);
         static IEnumerable<Type> _eventSubscribers = null!;
+        /// <summary>
+        /// 继承 IEventSubscriber的集合
+        /// </summary>
         static IEnumerable<Type> EventSubscribers
         {
             get
@@ -53,6 +27,13 @@ namespace WebApiTools
                     !x.IsAbstract && x.IsPublic && x.IsClass && x.IsToGenericInterface(InterfaceEventSubscriber));
             }
         }
+
+        /// <summary>
+        /// 判断当前类型是否是指定类型
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="baseInterface"></param>
+        /// <returns></returns>
         static bool IsToGenericInterface(this Type type, Type baseInterface)
         {
             if (type == null) return false;
@@ -60,6 +41,58 @@ namespace WebApiTools
             bool result = type.GetInterfaces().Any(x => x.IsGenericType && x.GetGenericTypeDefinition() == baseInterface);
             return result;
         }
+        /// <summary>
+        /// 服务注册
+        /// </summary>
+        /// <param name="services"></param>
+        public static void Configure(WebApplicationBuilder builder)
+        {
+            var configuration = builder.Configuration;
+            var _env = builder.Environment.EnvironmentName;
+
+           
+            builder.Services.AddScoped<Publisher>();
+            builder.Services.AddSingleton<ITestServices, TestServices>();
+            // 配置自动注入eventBus
+            ConfigureEventBus(builder.Services);
+            ConfigureAgileConfig(configuration, _env);
+        }
+
+        #region Event_Bus
+        public static void ConfigureEventBus(IServiceCollection services)
+        {
+            //注册EventHanders
+            foreach (var subscriberType in EventSubscribers)
+            {
+                //存在一个订阅者订阅多个事件的情况:
+                var baseTypes = subscriberType.GetInterfaces().Where(x => x.IsGenericType && x.GetGenericTypeDefinition() == InterfaceEventSubscriber).ToArray();
+                foreach (var baseType in baseTypes)
+                {
+                    services.AddScoped(baseType, subscriberType);
+                }
+            }
+        }
+
+
+        #endregion
+        #region AgileConfig
+        public static void ConfigureAgileConfig(IConfigurationManager configurationManager,string env)
+        {
+
+            configurationManager.AddAgileConfig(z =>
+            {
+                // 根据launchSettings 自适应配置 apgileconfig的env
+                z.ENV = env switch
+                {
+                    "Development" => "DEV",
+                    "Staging" => "STG",
+                    "Production" => "PROD",
+                    _ => "TEST"
+                }; ConfigClientOptions.FromConfiguration(configurationManager);
+            });
+        }
+
+        #endregion
 
     }
 }
--- WebApiTools/Program.cs
+++ WebApiTools/Program.cs
@@ -1,3 +1,5 @@
+using AgileConfig.Client;
+
 using Serilog;
 using Serilog.Events;
 
@@ -5,51 +7,45 @@ using WebApiTools;
 using WebApiTools.TestService;
 using WebApiTools.Tools.Event_Bus;
 
-internal class Program
+
+try
 {
-    private static void Main(string[] args)
-    {
+    var baseDir = AppDomain.CurrentDomain.BaseDirectory;
+    Log.Logger = new LoggerConfiguration()
+      .MinimumLevel.Information()
+      .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+      .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Warning)
+      .Enrich.FromLogContext()
+      .WriteTo.Logger(log => log.Filter.ByIncludingOnly(evt => evt.Level == LogEventLevel.Information).WriteTo.File($"{baseDir}/Logs/{DateTime.Now.ToString("yyyy-MM-dd", System.Globalization.CultureInfo.CurrentCulture) + "/info.txt"}", fileSizeLimitBytes: 8388608000, rollingInterval: RollingInterval.Day), LogEventLevel.Information)
+      .WriteTo.Logger(log => log.Filter.ByIncludingOnly(evt => evt.Level == LogEventLevel.Warning).WriteTo.File($"{baseDir}/Logs/{DateTime.Now.ToString("yyyy-MM-dd", System.Globalization.CultureInfo.CurrentCulture) + "/warning.txt"}", fileSizeLimitBytes: 83886080, rollingInterval: RollingInterval.Day), LogEventLevel.Warning)
+      .WriteTo.Logger(log => log.Filter.ByIncludingOnly(evt => evt.Level == LogEventLevel.Fatal).WriteTo.File($"{baseDir}/Logs/{DateTime.Now.ToString("yyyy-MM-dd", System.Globalization.CultureInfo.CurrentCulture) + "/fatal.txt"}", fileSizeLimitBytes: 83886080, rollingInterval: RollingInterval.Day), LogEventLevel.Fatal)
+      .WriteTo.Logger(log => log.Filter.ByIncludingOnly(evt => evt.Level == LogEventLevel.Error).WriteTo.File($"{baseDir}/Logs/{DateTime.Now.ToString("yyyy-MM-dd", System.Globalization.CultureInfo.CurrentCulture) + "/error.txt"}", fileSizeLimitBytes: 83886080, rollingInterval: RollingInterval.Day), LogEventLevel.Error)
+      .WriteTo.Async(c => c.Console())
+      .CreateLogger();
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Services.AddControllers();
+    builder.Services.AddSwaggerGen();
+    //builder.Services.AddAgileConfig();
+    #region 服务注册
+   
+    ConfigureServices.Configure(builder);
+    #endregion
 
-        try
-        {
-            var baseDir = AppDomain.CurrentDomain.BaseDirectory;
-            Log.Logger = new LoggerConfiguration()
-              .MinimumLevel.Information()
-              .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-              .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Warning)
-              .Enrich.FromLogContext()
-              .WriteTo.Logger(log => log.Filter.ByIncludingOnly(evt => evt.Level == LogEventLevel.Information).WriteTo.File($"{baseDir}/Logs/{DateTime.Now.ToString("yyyy-MM-dd", System.Globalization.CultureInfo.CurrentCulture) + "/info.txt"}", fileSizeLimitBytes: 8388608000, rollingInterval: RollingInterval.Day), LogEventLevel.Information)
-              .WriteTo.Logger(log => log.Filter.ByIncludingOnly(evt => evt.Level == LogEventLevel.Warning).WriteTo.File($"{baseDir}/Logs/{DateTime.Now.ToString("yyyy-MM-dd", System.Globalization.CultureInfo.CurrentCulture) + "/warning.txt"}", fileSizeLimitBytes: 83886080, rollingInterval: RollingInterval.Day), LogEventLevel.Warning)
-              .WriteTo.Logger(log => log.Filter.ByIncludingOnly(evt => evt.Level == LogEventLevel.Fatal).WriteTo.File($"{baseDir}/Logs/{DateTime.Now.ToString("yyyy-MM-dd", System.Globalization.CultureInfo.CurrentCulture) + "/fatal.txt"}", fileSizeLimitBytes: 83886080, rollingInterval: RollingInterval.Day), LogEventLevel.Fatal)
-              .WriteTo.Logger(log => log.Filter.ByIncludingOnly(evt => evt.Level == LogEventLevel.Error).WriteTo.File($"{baseDir}/Logs/{DateTime.Now.ToString("yyyy-MM-dd", System.Globalization.CultureInfo.CurrentCulture) + "/error.txt"}", fileSizeLimitBytes: 83886080, rollingInterval: RollingInterval.Day), LogEventLevel.Error)
-              .WriteTo.Async(c => c.Console())
-              .CreateLogger();
-            var builder = WebApplication.CreateBuilder(args);
-            builder.Services.AddControllers();
-            builder.Services.AddSwaggerGen();
-            #region 服务注册
-            ConfigureServices.Configure(builder.Services);
-            #endregion
-
-            var app = builder.Build();
+    var app = builder.Build();
 #if DEBUG
-            app.UseSwagger();
-            app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 #endif
-            app.UseHttpsRedirection();
-            app.UseAuthorization();
-            app.MapControllers();
-            app.Run();
-        }
-        catch (Exception ex)
-        {
-            Log.Fatal(ex, "Host terminated unexpectedly!");
-        }
-        finally
-        {
-            Log.CloseAndFlush();
-        }
-    }
-
-    
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    app.MapControllers();
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly!");
+}
+finally
+{
+    Log.CloseAndFlush();
 }
